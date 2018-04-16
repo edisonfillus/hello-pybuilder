39920744c60fc1444391454d24f2a089026dbef9
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-04-15 21:48:29 -0300

    Include SonarQube Analysis Integration

--- build.py
+++ build.py
@@ -1,21 +1,29 @@
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, task, depends
+from pybuilder.errors import BuildFailedException
 
 #Core Plugins
-use_plugin("python.core") 
+use_plugin("python.core")
 use_plugin("python.install_dependencies") #pyb install_dependencies
 
-#Test Plugins
+#QA Plugins
 use_plugin("python.unittest")
 use_plugin("python.coverage")
+use_plugin("python.sonarqube")
+
 
 #Package Plugins
 use_plugin("python.distutils") #Create a setup.py
 
-name = "tutorial-env"
-default_task = "publish"
+name = "hello-pybuilder"
+default_task = ["clean","install_dependencies","publish",'run_sonar_analysis']
 
 
-@init
+# Coverage configuration:
+#coverage_allow_non_imported_modules = "True"
+#coverage_exceptions = [ 'file.py' ]
+
+
+@init(environments='venv')
 def set_properties(project):
     pass
 
@@ -25,3 +33,12 @@ def initialize(project):
     #project.depends_on("awesome-library") #Dependencies for Build and Runtime
     project.build_depends_on_requirements("requirements-dev.txt") #Dependencies on File Just for Build
     project.depends_on_requirements("requirements.txt") #Dependencies on File For Build and Runtime
+
+'''Overring run_sonar_analysis in order to lunch sonar-scanner instead of sonar-runner'''
+@task("run_sonar_analysis", description="Launches sonar-scanner for analysis.")
+@depends("analyze")
+def run_sonar_analysis():
+    import subprocess
+    result = subprocess.run(['sonar-scanner', '-X'], shell=True) #sonar-scanner must have a link on /bin directory
+    if result.returncode != 0:
+        raise BuildFailedException("Sonar analysis failed.")
